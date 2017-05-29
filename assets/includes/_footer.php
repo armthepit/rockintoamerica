8195a2cd38ec8a094e454351f61e0bf919a43e65
John Nowinsky <armthepit@armthepit.com>
2017-05-29 16:12:00 -0500

    divide index.php into a header and footer template file.

---
+++ assets/includes/_footer.php
@@ -0,0 +1,34 @@
+            <div class="col-sm-12 col-md-9">
+                test
+            </div>
+        </div>
+    </div>
+    <footer>
+        <script language="JavaScript">
+        <!--
+
+        //Script created by Ronny Drappier, http://sipreal.com
+        //Visit http://javascriptkit.com for this script
+
+        today = new Date();
+        y0 = today.getFullYear();
+
+        // end hiding --->
+        </script>
+        Site Designed &amp; Maintained By
+        <a target="_blank" href="http://www.websitesforeveryoneelse.com">
+        &nbsp;Websites For Everyone Else&nbsp;
+    </a> (c)2011 -
+        <script language="JavaScript">
+        <!--- Hide from old browsers
+        document.write(y0);
+        // end hiding --->
+        </script>
+    </footer>
+    <!-- JQuery  -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ assets/includes/_header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Online CD Store">
+    <meta name="keywords" content="Rockin To America">
+    <title>Rockin To America</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Latest Font Awesome -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css" media="screen">
+</head>
+
+<body>
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 hidden-sm hidden-xs">
+                    <div class="well">
+                        <img src="assets/images/logo.png" alt="Rockin To America Logo" class="center-block">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- This the navigation bar. -->
+    <nav id="mainmenu" class="navbar navbar-default navbar-fixed-static" role="navigation">
+        <div class="container-fluid nav-container">
+            <div class="navbar-header">
+                <span class="text-nowrap">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-menubuilder">
+                    <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand visible-xs" href="#">Rockin To America</a>
+                </span>
+            </div>
+            <div class="collapse navbar-collapse navbar-menubuilder">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><i class="fa fa-lock" aria-hidden="true"></i> Log In</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="content">
+        <div class="row">
+            <div class="col-sm-12 col-md-3">
+                Genres
+            </div>
